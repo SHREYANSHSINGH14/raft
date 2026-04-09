e546ee52cedaa0ed55d3edb30e5bd891c2da8572
SHREYANSHSINGH14 <shreyanshsinghchouhan1@gmail.com>
2026-04-09 19:22:07 +0000

    feat: implement AppendEntries and RequestVote methods for Raft protocol

---
+++ raft/append_entries.go
@@ -0,0 +1,116 @@
+package raft
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/SHREYANSHSINGH14/raft/types"
+	"github.com/rs/zerolog"
+)
+
+func (p *Server) AppendEntries(ctx context.Context, args *types.AppendEntriesArgs) (*types.AppendEntriesResponse, error) {
+	if strings.TrimSpace(args.LeaderId) == "" {
+		err := fmt.Errorf("leader id is empty")
+		zerolog.Ctx(ctx).Error().Err(err).Msg("leader id is empty")
+		return nil, err
+	}
+
+	currentTerm, err := p.store.GetCurrentTerm(ctx)
+	if err != nil {
+		zerolog.Ctx(ctx).Error().Err(err).Msgf("append entries db err: %+w", err)
+		return nil, err
+	}
+
+	if args.Term < uint64(currentTerm) {
+		return &types.AppendEntriesResponse{
+			Term:    uint64(currentTerm),
+			Success: false,
+		}, nil
+	}
+
+	if args.Term > uint64(currentTerm) {
+		err := p.store.SetCurrentTerm(ctx, uint(args.Term))
+		if err != nil {
+			zerolog.Ctx(ctx).Error().Err(err).Msgf("append entries db err: %+w", err)
+			return nil, err
+		}
+		currentTerm = uint(args.Term)
+
+		err = p.store.SetVotedFor(ctx, "")
+		if err != nil {
+			zerolog.Ctx(ctx).Error().Err(err).Msgf("append entries db err: %+w", err)
+			return nil, err
+		}
+	}
+
+	prevLog, err := p.store.GetLogByIndex(ctx, uint(args.PrevLogIndex))
+	if err != nil {
+		zerolog.Ctx(ctx).Error().Err(err).Msgf("append entries db err: %+w", err)
+		return nil, err
+	}
+
+	if prevLog != nil && prevLog.Term != args.PrevLogTerm {
+		return &types.AppendEntriesResponse{
+			Term:    uint64(currentTerm),
+			Success: false,
+		}, nil
+	}
+
+	// Here we are not checking for log consistency and directly appending the logs because in this implementation we are not supporting log replication optimization
+	// and we are just sending all the logs from nextIndex to end of log to the follower and if there is any inconsistency then it will be resolved in next append entries
+	// call when leader will again send all the logs from nextIndex to end of log and in that case follower will just overwrite the inconsistent log
+	// with the correct log from leader
+
+	// Append Logs Optimization Phases
+
+	// Phase 1 - Leader sends the new logs to the followers and they append the new logs to their log store
+	// leader will keep decrementing the nextIndex for that follower until it finds the right log index from which
+	// it should send the logs to that follower and once it finds that index then it will update the nextIndex for
+	// that follower to be that index + 1 and then in next append entries call it will send the logs from that index + 1 to end of log (DONE)
+
+	// Phase 2 - We response is unsuccessful because of log inconsistency, then follower will also send the conflicting log index in response
+	// follower will skip to starting of term of that conflicting log index, then leader will update the nextIndex for that follower to be that conflicting log index
+	// and then in next append entries call it will send the logs from that conflicting log index to end of log (TODO)
+
+	// If we are here then it means the logs are consistent till prevLogIndex and prevLogTerm, so we can safely truncate the logs from prevLogIndex + 1 and append the new logs
+	// This is because if there is any inconsistency then it will be in the logs after prevLogIndex and prevLogTerm, so we can just truncate the logs from there and append the new logs from leader which are correct
+	// This is simple way to resolve log inconsistency without having to find the exact conflicting log index and it is also efficient because in real world scenario we won't have many inconsistent logs and even if
+	// we have many inconsistent logs then it means there is some issue with the leader and in that case we can just truncate all the logs after prevLogIndex and append the new logs from leader which are correct
+	err = p.store.TruncateLogs(ctx, uint(args.PrevLogIndex)+1)
+	if err != nil {
+		zerolog.Ctx(ctx).Error().Err(err).Msgf("append entries db err: %+w", err)
+		return nil, err
+	}
+
+	// Append the new logs to the log store
+	if len(args.Entries) > 0 {
+		err = p.store.AppendLogs(ctx, args.Entries)
+		if err != nil {
+			zerolog.Ctx(ctx).Error().Err(err).Msgf("append entries db err: %+w", err)
+			return nil, err
+		}
+	}
+
+	if args.LeaderCommit > uint64(p.getCommitIndex()) {
+		lastLogIdx, err := p.store.GetLastLogIndex(ctx)
+		if err != nil {
+			zerolog.Ctx(ctx).Error().Err(err).Msgf("append entries db err: %+w", err)
+			return nil, err
+		}
+
+		minCommitIndex := min(args.LeaderCommit, uint64(lastLogIdx))
+		p.setCommitIndex(uint(minCommitIndex))
+	}
+
+	if p.getLeaderID() != args.LeaderId {
+		p.setLeaderID(args.LeaderId)
+	}
+
+	p.electionTimeoutCh <- struct{}{} // reset election timeout
+
+	return &types.AppendEntriesResponse{
+		Term:    uint64(currentTerm),
+		Success: true,
+	}, nil
+}
---
+++ raft/request_vote.go
@@ -0,0 +1,96 @@
+package raft
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/SHREYANSHSINGH14/raft/types"
+	"github.com/rs/zerolog"
+)
+
+func (p *Server) RequestVote(ctx context.Context, args *types.RequestVoteArgs) (*types.RequestVoteResponse, error) {
+	if strings.TrimSpace(args.CandidateId) == "" {
+		err := fmt.Errorf("candidate id is empty")
+		zerolog.Ctx(ctx).Error().Err(err).Msg("candidate id is empty")
+		return nil, err
+	}
+
+	currentTerm, err := p.store.GetCurrentTerm(ctx)
+	if err != nil {
+		zerolog.Ctx(ctx).Error().Err(err).Msgf("request vote db err: %+w", err)
+		return nil, err
+	}
+
+	if args.Term < uint64(currentTerm) {
+		return &types.RequestVoteResponse{
+			Term:        uint64(currentTerm),
+			VoteGranted: false,
+		}, nil
+	}
+
+	// Here we are not keeping check for equal term because even if candidate's term is equal to current term
+	// then it would mean this is probably a double request from same candidate or from different candidate but in same term
+	// In both cases we can just ignore the request and return false because we have already voted for some candidate in this term
+	// that happens at voteFor check below where we check if votedFor is empty or candidateId
+	if args.Term > uint64(currentTerm) {
+		err := p.store.SetCurrentTerm(ctx, uint(args.Term))
+		if err != nil {
+			zerolog.Ctx(ctx).Error().Err(err).Msgf("request vote db err: %+w", err)
+			return nil, err
+		}
+
+		currentTerm = uint(args.Term)
+
+		err = p.store.SetVotedFor(ctx, "")
+		if err != nil {
+			zerolog.Ctx(ctx).Error().Err(err).Msgf("request vote db err: %+w", err)
+			return nil, err
+		}
+	}
+
+	votedFor, err := p.store.GetVotedFor(ctx)
+	if err != nil {
+		zerolog.Ctx(ctx).Error().Err(err).Msgf("request vote db err: %+w", err)
+		return nil, err
+	}
+
+	if votedFor != "" && votedFor != args.CandidateId {
+		return &types.RequestVoteResponse{
+			Term:        uint64(currentTerm),
+			VoteGranted: false,
+		}, nil
+	}
+
+	lastLog, err := p.store.GetLastLogEntry(ctx)
+	if err != nil {
+		zerolog.Ctx(ctx).Error().Err(err).Msgf("request vote db err: %+w", err)
+		return nil, err
+	}
+
+	// Raft determines which of two logs is more up-to-date
+	// by comparing the index and term of the last entries in the
+	// logs. If the logs have last entries with different terms, then
+	// the log with the later term is more up-to-date. If the logs
+	// end with the same term, then whichever log is longer is
+	// more up-to-date.
+	if lastLog != nil {
+		if args.LastLogTerm < uint64(lastLog.Term) || (args.LastLogTerm == uint64(lastLog.Term) && args.LastLogIndex < lastLog.Index) {
+			return &types.RequestVoteResponse{
+				Term:        uint64(currentTerm),
+				VoteGranted: false,
+			}, nil
+		}
+	}
+
+	err = p.store.SetVotedFor(ctx, args.CandidateId)
+	if err != nil {
+		zerolog.Ctx(ctx).Error().Err(err).Msgf("request vote db err: %+w", err)
+		return nil, err
+	}
+
+	return &types.RequestVoteResponse{
+		Term:        uint64(currentTerm),
+		VoteGranted: true,
+	}, nil
+}
