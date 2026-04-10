9ccb648bb83ae9af22be687313ce6c40b5d4ca1f
SHREYANSHSINGH14 <shreyanshsinghchouhan1@gmail.com>
2026-04-10 20:47:31 +0000

    feat: add panic recovery mechanism in main function

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "github.com/SHREYANSHSINGH14/raft/cmd"
+import (
+	"fmt"
+	"runtime/debug"
+
+	"github.com/SHREYANSHSINGH14/raft/cmd"
+)
 
 func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered from panic:", r)
+			debug.PrintStack()
+		}
+	}()
 	cmd.Execute()
 }
